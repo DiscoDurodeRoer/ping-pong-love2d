8cfa8fd33151891b0ec4e44b07693cbc571f696f
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2018-07-20 00:17:26 +0200

    Sonidos y puntuación
    Añadimos sonidos y puntuación

--- ping-pong/main.lua
+++ ping-pong/main.lua
@@ -3,27 +3,16 @@ function love.load()
     love.window.setTitle("Pong")
     love.window.setMode(1600, 800)
 
-    barritaJ1 = {}
-    barritaJ1.x = 50
-    barritaJ1.y = 300
-    barritaJ1.ancho = 50
-    barritaJ1.altura = 250
-    barritaJ1.velocidad = 5
+    puntuacionJ1 = 0
+    puntuacionJ2 = 0
 
-    barritaJ2 = {}
-    barritaJ2.x = 1500
-    barritaJ2.y = 300
-    barritaJ2.ancho = 50
-    barritaJ2.altura = 250
-    barritaJ2.velocidad = 5
+    reset("J1")
 
-    pelota = {}
-    pelota.x = love.graphics.getWidth() / 2
-    pelota.y = love.graphics.getHeight() / 2
-    pelota.radio = 20
-    pelota.derecha = false
-    pelota.arriba = false
-    pelota.velocidad = 5
+    fuente = love.graphics.newFont(70)
+
+    sonidoPong = love.audio.newSource("pong.wav")
+
+    sonidoPunto = love.audio.newSource("grito.mp3")
 
 end
 
@@ -38,7 +27,13 @@ end
 
 function love.draw()
 
+  love.graphics.setFont(fuente)
+
   love.graphics.setColor(255, 255, 255)
+
+  love.graphics.print(puntuacionJ1, (love.graphics.getWidth() / 2)-200, 50 )
+  love.graphics.print(puntuacionJ2, (love.graphics.getWidth() / 2)+200, 50 )
+
   love.graphics.rectangle("fill", barritaJ1.x, barritaJ1.y, barritaJ1.ancho, barritaJ1.altura)
   love.graphics.rectangle("fill", barritaJ2.x, barritaJ2.y, barritaJ2.ancho, barritaJ2.altura)
 
@@ -82,6 +77,7 @@ function rebotarPelota(pelota)
       and (pelota.x) <= (barritaJ1.x + barritaJ1.ancho)
       and (pelota.y - pelota.radio) <= (barritaJ1.y + barritaJ1.altura)
       and (pelota.y - pelota.radio) >= (barritaJ1.y)   then
+        love.audio.play(sonidoPong)
       pelota.arriba = false
   end
 
@@ -89,6 +85,7 @@ function rebotarPelota(pelota)
       and (pelota.x) <= (barritaJ1.x + barritaJ1.ancho)
       and (pelota.y + pelota.radio) >= (barritaJ1.y)
       and (pelota.y + pelota.radio) <= (barritaJ1.y + barritaJ1.altura) then
+      love.audio.play(sonidoPong)
       pelota.arriba = true
   end
 
@@ -96,6 +93,7 @@ function rebotarPelota(pelota)
       and (pelota.x) <= (barritaJ2.x + barritaJ2.ancho)
       and (pelota.y - pelota.radio) <= (barritaJ2.y + barritaJ2.altura)
       and (pelota.y + pelota.radio) >= (barritaJ2.y)   then
+        love.audio.play(sonidoPong)
       pelota.arriba = false
   end
 
@@ -103,6 +101,7 @@ function rebotarPelota(pelota)
       and (pelota.x) <= (barritaJ2.x + barritaJ2.ancho)
       and (pelota.y + pelota.radio) >= (barritaJ2.y)
       and (pelota.y + pelota.radio) <= (barritaJ2.y + barritaJ2.altura) then
+        love.audio.play(sonidoPong)
       pelota.arriba = true
   end
 
@@ -110,6 +109,7 @@ function rebotarPelota(pelota)
       and (pelota.x - pelota.radio) >= (barritaJ1.x)
       and (pelota.y ) >= (barritaJ1.y)
       and (pelota.y ) <= (barritaJ1.y + barritaJ1.altura) then
+        love.audio.play(sonidoPong)
       pelota.derecha = true
       pelota.velocidad = pelota.velocidad + 1
   end
@@ -118,16 +118,21 @@ function rebotarPelota(pelota)
     and (pelota.x + pelota.radio) <= (barritaJ2.x + barritaJ2.ancho)
     and (pelota.y ) >= (barritaJ2.y)
     and (pelota.y ) <= (barritaJ2.y + barritaJ2.altura) then
+      love.audio.play(sonidoPong)
     pelota.derecha = false
     pelota.velocidad = pelota.velocidad + 1
   end
 
   if (pelota.x + pelota.radio) >= love.graphics.getWidth() then
     reset("J1")
+    love.audio.play(sonidoPunto)
+    puntuacionJ1 = puntuacionJ1 + 1
   end
 
   if (pelota.x - pelota.radio) <= 0 then
     reset("J2")
+    love.audio.play(sonidoPunto)
+    puntuacionJ2 = puntuacionJ2 + 1
   end
 
   if (pelota.y + pelota.radio) >= love.graphics.getHeight() then
@@ -141,18 +146,34 @@ function rebotarPelota(pelota)
 end
 
 
-function reset(punto)
+function reset(jugador_punto)
 
+  barritaJ1 = {}
+  barritaJ1.x = 50
+  barritaJ1.y = 300
+  barritaJ1.ancho = 50
+  barritaJ1.altura = 250
+  barritaJ1.velocidad = 5
+
+  barritaJ2 = {}
+  barritaJ2.x = 1500
+  barritaJ2.y = 300
+  barritaJ2.ancho = 50
+  barritaJ2.altura = 250
+  barritaJ2.velocidad = 5
+
+  pelota = {}
   pelota.x = love.graphics.getWidth() / 2
   pelota.y = love.graphics.getHeight() / 2
   pelota.radio = 20
-  pelota.derecha = false
+
+  if jugador_punto == "J1" then
+    pelota.derecha = false
+  else
+    pelota.derecha = true
+  end
 
   pelota.arriba = false
   pelota.velocidad = 5
-
-  love.graphics.setColor(255, 0, 0)
-  love.graphics.circle("fill", pelota.x, pelota.y, pelota.radio)
-
 
 end
